67875e0d4a60b11c51ae9c0f76b025e1343bbfbd
Ebrahim-hamdy <ebrahim0hamdy@gmail.com>
2019-05-12 17:14:32 +0300

    chore: add heruku config

--- src/components/Video/Video.component.jsx
+++ src/components/Video/Video.component.jsx
@@ -7,8 +7,9 @@ import VideoList from './List/VideoList';
 const VideoComponent = () => {
   let [url, setUrl] = useState('');
   let [items, setItems] = useState([
-    { url: 'https://www.youtube.com/watch?v=yNL9_nvF4Ks', date: '08.05.2019' },
-    { url: 'https://www.youtube.com/watch?v=lf8TsRFuks8', date: '10.05.2019' },
+    { url: 'https://www.youtube.com/watch?v=SsoOG6ZeyUI', date: '5/12/2019, 1:02:17 PM' },
+    { url: 'https://www.youtube.com/watch?v=EUSsEch7dZA', date: '5/11/2019, 3:02:17 PM' },
+    { url: 'https://vimeo.com/97107551', date: '5/12/2019, 9:02:17 PM' },
   ]);
 
   const onSearch = value => {
